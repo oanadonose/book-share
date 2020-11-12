fc9e8934798d9e1390a666a3111c4a4cf27b5060
Oana Donose (donoseo) <donoseo@coventry.ac.uk>
2020-11-12 15:59:37 +0000

    Revert "Feature/pl 3 jsonschema and jsdocs"

--- docs/openapi/schemas/schemas.schema.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-let bookSchema = JSON.parse(fs.readFileSync('docs\\openapi\\schemas\\book.json'));
-delete bookSchema['definitions'];
-
-let userSchema = JSON.parse(fs.readFileSync('docs\\openapi\\schemas\\user.json'));
-delete userSchema['definitions'];
-
-export {bookSchema, userSchema};
