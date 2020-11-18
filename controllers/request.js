6ea652fdb5a3b95d708af7f01bcf42647b3c6086
Oana Donose <donoseo@coventry.ac.uk>
2020-11-18 15:57:08 +0000

    added route to get requests made and received based on user id param

--- controllers/request.js
+++ controllers/request.js
@@ -5,7 +5,6 @@
  * @requires jsonwebtoken
  * @requires models/request
  */
-import mongoose from 'mongoose';
 import Request from '../models/request.js';
 import Book from '../models/book.js';
 import Message from '../models/message.js';
@@ -29,25 +28,21 @@ export const addRequest = async (req, res) => {
 	const bookId = req.body.bookId;
 	const userId = req.user.id;
 	let message;
-	if(req.body.message) {
-		message = new Message({
-			userId,
-			text: req.body.message
-		});
-		try {
-			message.save();
-		} catch (err) {
-			res.status(500).send(err);
-		}
-	}
-	//getbookinfo
-	const request = new Request({
-		bookId,
-		userId,
-		messages:[message],
-		status: 'open'
-	});
 	try {
+		if(req.body.message) {
+			message = new Message({
+				userId,
+				text: req.body.message
+			});
+			message.save();
+		}
+		//getbookinfo
+		const request = new Request({
+			bookId,
+			userId,
+			messages:[message],
+			status: 'open'
+		});
 		request.save();
 		const result = await Book.findByIdAndUpdate(bookId, { $push: {'requests': request} }, options);
 		res.status(200).send({request, result});
@@ -66,20 +61,103 @@ export const addMessage = async (req, res) => {
 	console.log('req.body', req.body);
 	console.log('req.headers', req.headers);
 	console.log('req.user.id', req.user.id);
-	if(req.body.message) {
-		const message = new Message({
-			text: req.body.message,
-			userId: req.user.id
-		});
-		try {
+	try {
+		if(req.body.message) {
+			const message = new Message({
+				text: req.body.message,
+				userId: req.user.id
+			});
 			message.save();
 			const result = await Request.findByIdAndUpdate(req.body.requestId, { $push: {'messages': message } }, options);
 			res.status(200).send(result);
-		} catch (err) {
-			console.log('err', err);
-			res.status(500).send({message: 'error saving message'});
+		} else {
+			res.status(403).send({message: 'message cannot be empty.'});
 		}
-	} else {
-		res.status(403).send({message: 'message cannot be empty.'});
+	} catch (err) {
+		res.status(500).send({err});
+	}
+};
+/**
+ * Function to close request
+ * @function
+ * @param {express.Request} req
+ * @param {express.Response} res
+ */
+export const closeRequest = async (req, res) => {
+	console.log('req.body', req.body);
+	console.log('req.user.id', req.user.id);
+	try {
+		const request = await Request.findById(req.body.requestId).populate({ //TODO: change path to book in moswl
+			path: 'bookId',
+			options: { lean: true }
+		}).lean().exec();
+		console.log(request,'request');
+		if(request.bookId.user == req.user.id || req.user.id == request.userId) {
+			const result = await Request.findByIdAndUpdate(req.body.requestId, {'status': 'closed'}, {...options});
+			res.status(200).send(result);
+		} else {
+			res.status(403).send({message: 'no permission to close request',
+				'request.bookId.user':request.bookId.user, 'request.userId':request.userId, 'req.user.id':req.user.id });
+		}
+	} catch(err) {
+		console.log('err', err);
+		res.status(500).send({message: 'error closing request'});
+	}
+};
+/**
+ * Function to view requests MADE by user
+ * @function
+ * @param {express.Request} req
+ * @param {express.Response} res
+ */
+export const getUserMadeRequests = async (req, res) => {
+	console.log('req.body', req.body);
+	console.log('req.params.userid', req.params.userid)
+	console.log('req.user.id', req.user.id);
+	try {
+		const requests = await Request.find({userId: req.user.id})
+			.populate({
+				path: 'messages',
+				options: { lean: true }
+			})
+			.lean().exec();
+		console.log(requests);
+		res.status(200).send(requests);
+	} catch (err) {
+		console.log(err);
+		res.status(500).send(err);
+	}
+};
+
+/**
+ * Function to view requests RECEIVED by user
+ * @function
+ * @param {express.Request} req
+ * @param {express.Response} res
+ */
+export const getUserReceivedRequests = async (req, res) => {
+	console.log('req.params.userid', req.params.userid);
+	const requests = [];
+	try {
+		const books = await Book.find({user: req.params.userid})
+			.populate({
+				path: 'requests',
+				populate: {
+					path: 'messages',
+					model: 'Message'
+				},
+				options: { lean: true }
+			})
+			.lean()
+			.exec();
+		for(let book of books) {
+			if(book.requests.length) {
+				book.requests.forEach(request => requests.push(request));
+			}
+		}
+		res.status(200).send(requests);
+	} catch (err) {
+		console.log(err);
+		res.status(500).send(err);
 	}
 };
--- routes/requestRouter.js
+++ routes/requestRouter.js
@@ -6,7 +6,7 @@
 import express from 'express';
 import passport from 'passport';
 
-import { addRequest, addMessage } from '../controllers/request.js';
+import { addRequest, addMessage, closeRequest, getUserMadeRequests, getUserReceivedRequests } from '../controllers/request.js';
 
 /**
  * Express router to mount request related routes on.
@@ -25,6 +25,13 @@ requestRouter.get('/', (req, res) => {
 requestRouter.post('/add', passport.authenticate('jwt', { session: false }), addRequest);
 
 requestRouter.post('/reply', passport.authenticate('jwt', { session: false }), addMessage);
+
+requestRouter.post('/close', passport.authenticate('jwt', { session: false }), closeRequest);
+
+requestRouter.get('/:userid/made', passport.authenticate('jwt', { session: false }), getUserMadeRequests);
+
+requestRouter.get('/:userid/received', passport.authenticate('jwt', { session: false }), getUserReceivedRequests);
+
 
 
 export default requestRouter;
