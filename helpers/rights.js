e7a8ec7dac105079e5e3063d97af76cff594f5b9
Oana Donose <donoseo@coventry.ac.uk>
2020-12-13 18:18:29 +0000

    added access control middleware to routes

--- helpers/rights.js
+++ helpers/rights.js
@@ -9,4 +9,31 @@ const owns = (loggedUserId, bookUserId) => {
 	else return false;
 };
 
-export default owns;
+import {getUserRole} from '../helpers/users.js';
+import roles from '../helpers/roles.js';
+
+const can = (options) => {
+	return async (req,res,next) => {
+		let id;
+		if(!req.user) {
+			id = '5fd5f3ec85d00e4a2496f68f';
+		}
+		else {
+			id = req.user.id;
+		}
+		const role = await getUserRole(id);
+		const action = options.action;
+		const resource = options.resource;
+		try {
+			const permission = roles.can(role)[action](resource);
+			console.log('here', permission.granted);
+			if(permission.granted) next();
+			else throw new Error('permission denied');
+		} catch (err) {
+			if(err.message == 'permission denied') res.status(401).send(err.message);
+			else throw err;
+		}
+	};
+};
+
+export {owns, can};
